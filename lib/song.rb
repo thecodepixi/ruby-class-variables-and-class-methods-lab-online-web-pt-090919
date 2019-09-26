b42c7305278d9cdf364f013300eec3bf24bf262a
Emily Harber <emily.a.harber@gmail.com>
2019-09-26 04:00:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   def self.artist_count 
     artist_count = {} 
     @@artists.each do |artist|
-      if !artist_count.has_key(artist)
+      if !artist_count.has_key?(artist)
         artist_count[artist] = 1 
       else 
         artist_count[artist] += 1 
