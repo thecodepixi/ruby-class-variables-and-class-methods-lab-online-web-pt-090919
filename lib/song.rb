fdd669c018499aa0c4f790500ace0bc04cd131a9
Emily Harber <emily.a.harber@gmail.com>
2019-09-26 03:58:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,14 @@ class Song
   
   def self.genre_count 
     genre_count = {} 
-    
+    @@genres.each do |genre|
+      if !genre_count.include?(genre)
+        genre_count[genre] = 1 
+      else 
+        genre_count[genre] += 1 
+      end 
+    end 
+    genre_count 
   end 
   
   def self.artist_count 
